5ca8b833e309cdf4fa8877321559d7fbc2aad3f3
chandanakonchada30 <24331a05e2@mvgrce.edu.in>
2026-02-13 00:12:51 +0530

    Add files via upload

---
+++ week6-2.py
@@ -0,0 +1,6 @@
+#24331A05E2
+#counts the character in a string
+string = input("Enter a string: ")
+char = input("Enter the character to count: ")
+count = string.count(char)
+print(f"The character",char,"appears",count ,"times.")
